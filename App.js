84794cff67d8767397ae81a8e1724e7ba646080e
Miguel Ángel Sandoval Sánchez <miguelsandoval20470266@gmail.com>
2023-10-10 09:24:11 -0600

    Versión incial. No incluye presentación ni funcionalidad

--- App.js
+++ App.js
@@ -1,20 +1,38 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Button, TextInput} from 'react-native';
+import { useState} from 'react'
 
 export default function App() {
+  
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+    <View style={styles.maincontainer}>
+      <View>
+        <Text>Peso de la pesona en kilogramos</Text>
+        <TextInput/>
+        <Text>Estatura de la persona en metros:</Text>
+        <TextInput/>
+      </View>
+      <View style={styles.controlsContainer}>
+        <Button title='Calcular'/>
+        <Button title='Limpiar'/>
+      </View>
+      <View>
+        <Text>Indice de masa corporal de la persona es:</Text>
+        <Text>28.9012</Text>
+        <Text>El estado nutricional de la person es:</Text>
+        <Text>Peso normal</Text>
+      </View>
       <StatusBar style="auto" />
     </View>
   );
 }
 
 const styles = StyleSheet.create({
-  container: {
+  maincontainer: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: '#cccccc',
     alignItems: 'center',
     justifyContent: 'center',
   },
+
 });
